f6a532eeb634d2bb614fcc97b82151e156504936
GauravKesh <gkrcoder@gmail.com>
2024-05-22 18:56:23 +0530

    accura

---
+++ index.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  var dropdown = document.querySelector(".dropdown");
+  var hamburgerMenu = document.querySelector(".hamburger-menu");
+
+  dropdown.classList.toggle("active");
+  hamburgerMenu.classList.toggle("active");
+}
+
+
